aae9e2f0e4846e4ee498dc27168e321ed6ac4b35
Arun Gupta <arun.gupta@gmail.com>
2017-10-06 09:21:42 -0700

    moving hard coded values to config

--- employees/src/main/java/org/javaee/samples/employees/Main.java
+++ employees/src/main/java/org/javaee/samples/employees/Main.java
@@ -7,20 +7,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Container container = new Container();
 
-        container.fraction(new DatasourcesFraction().jdbcDriver("com.mysql", (d) -> {
-            d.driverClassName("com.mysql.jdbc.Driver");
-            d.xaDatasourceClass("com.mysql.jdbc.jdbc2.optional.MysqlXADataSource");
-            d.driverModuleName("com.mysql");
-        }).dataSource("MySQLDS", (ds) -> {
-            ds.driverName("com.mysql");
-            ds.connectionUrl(System.getenv().getOrDefault("JDBC_URL", "jdbc:mysql://mysql:3306/guestbook?useSSL=false&autoReconnect=true"));
-            ds.userName(System.getenv().getOrDefault("DATASOURCE_USERNAME", "myuser"));
-            ds.password(System.getenv().getOrDefault("DATASOURCE_PASSWORD", "mypassword"));
-            ds.backgroundValidation(true);
-            ds.validConnectionCheckerClassName("org.jboss.jca.adapters.jdbc.extensions.mysql.MySQLValidConnectionChecker");
-            ds.validateOnMatch(true);
-            ds.checkValidConnectionSql("SELECT 1");
-        }));
+        container.fraction(new DatasourcesFraction().jdbcDriver("com.mysql").dataSource("ExampleDS"));
 
         // Start the container and deploy the default war
         container.start().deploy();
